5bd54f78b7f54fced1d53d5afedf903bb181085f
Yoshua Woo <yoshua.woo@gmail.com>
2020-10-16 20:23:59 +0200

    Some AI Head Movement Tweaking

--- Assets/#MyStuff/Scripts/AI/Behaviours/LookAtPlayerBehaviour.cs
+++ Assets/#MyStuff/Scripts/AI/Behaviours/LookAtPlayerBehaviour.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 [CreateAssetMenu( menuName = "AI Kit/Behaviours/LookAtPlayerBehaviour" )]
 public class LookAtPlayerBehaviour : Behaviour
@@ -7,6 +8,7 @@ public class LookAtPlayerBehaviour : Behaviour
     {
         NavMeshAgentStateMachine machine = _stateMachine as NavMeshAgentStateMachine;
 
-        machine.Head.LookAt( machine.Player.transform, Vector3.up );
+        // machine.Head.LookAt( machine.Player.transform, Vector3.up );
+        machine.Head.DOLookAt( machine.Player.transform.position, 0.08f, AxisConstraint.None, Vector3.up ).SetEase( Ease.InOutCirc );
     }
 }
--- Assets/#MyStuff/Scripts/AI/Exit Actions/ResetHeadRotationExitAction.cs
+++ Assets/#MyStuff/Scripts/AI/Exit Actions/ResetHeadRotationExitAction.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using DG.Tweening;
 
 [CreateAssetMenu( menuName = "AI Kit/Exit Actions/ResetHeadRotationExitAction" )]
 public class ResetHeadRotationExitAction : ExitAction
 {
     public override void Exit(StateMachine _stateMachine)
     {
-        _stateMachine.Head.localRotation = Quaternion.identity;
+        _stateMachine.Head.DOKill();
+        // _stateMachine.Head.localRotation = Quaternion.identity;
+        _stateMachine.Head.DOLocalRotate( Vector3.zero, 0.05f, RotateMode.Fast );
     }
 }
