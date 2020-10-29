42be255f7313ab4cdc4dfdc8333e354c1c7d2c8a
Yoshua Woo <yoshua.woo@gmail.com>
2020-10-29 12:48:38 +0100

    Some scriptableobject internal data that I tampered with to get behaviour instances to properly serialize (didn't work)

--- Assets/#MyStuff/Scripts/AI/Behaviours/LookAtTargetBehaviour.cs
+++ Assets/#MyStuff/Scripts/AI/Behaviours/LookAtTargetBehaviour.cs
@@ -7,7 +7,6 @@ public class LookAtTargetBehaviour : Behaviour
     public override void DoBehaviour(StateMachine _stateMachine)
     {
         NavMeshAgentStateMachine machine = _stateMachine as NavMeshAgentStateMachine;
-
         // machine.Head.LookAt( machine.LookAtTarget, Vector3.up );
         machine.Head.DOLookAt( machine.LookAtTarget.position, 0.08f, AxisConstraint.None, Vector3.up ).SetEase( Ease.InOutCirc );
     }
