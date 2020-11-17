bb5a59698e1dcca0ff8c851e7b9fbe25544d15d8
Yoshua Woo <yoshua.woo@gmail.com>
2020-11-17 18:38:56 +0100

    Prefab and Feedback Changes
    Put the Gun prefab on the Gun layer;
    tweaked feedback to be juicier, more playful;
    Added a rotation helper script for MMFeedbacks property controller since MMFeedbacks has no local rotation controller by default that works with my requirements;

---
+++ Assets/#MyStuff/Scripts/Shooting/Feedback/LocalRotationFeedback.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LocalRotationFeedback : MonoBehaviour
+{
+    public float xLocalRotation;
+
+    private Vector3 defaultLocalRotation;
+
+    private void Start()
+    {
+        defaultLocalRotation = transform.localRotation.eulerAngles;
+    }
+
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(xLocalRotation, defaultLocalRotation.y, defaultLocalRotation.z);
+    }
+}
