4fc598c4d8a20eea199d29d6a1909791394097b6
Yoshua Woo <yoshua.woo@gmail.com>
2020-11-26 16:08:47 +0100

    ESC to quit

--- Assets/#MyStuff/Scripts/Manager/GameManager.cs
+++ Assets/#MyStuff/Scripts/Manager/GameManager.cs
@@ -33,5 +33,10 @@ public class GameManager : MonoBehaviour
 		{
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		}
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
 	}
 }
