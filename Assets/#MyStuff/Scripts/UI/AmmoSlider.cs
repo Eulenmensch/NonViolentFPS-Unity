fe6f6b5821f94a8d84dc6c897503d402f3749702
Yoshua Woo <yoshua.woo@gmail.com>
2020-12-02 19:08:11 +0100

    Refactored Ammoslider to remove dependency to scene object

---
+++ Assets/#MyStuff/Scripts/UI/AmmoSlider.cs
@@ -0,0 +1,35 @@
+﻿using NonViolentFPS.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NonViolentFPS.UI
+{
+	public class AmmoSlider : MonoBehaviour
+	{
+		[SerializeField] private Slider ammoSlider;
+
+		private int currentGunAmmoCount;
+
+		private void OnEnable()
+		{
+			PlayerEvents.Instance.OnAmmoChanged += UpdateAmmoSliderPosition;
+			PlayerEvents.Instance.OnGunChanged += UpdateCurrentGunAmmoCount;
+		}
+
+		private void OnDisable()
+		{
+			PlayerEvents.Instance.OnAmmoChanged -= UpdateAmmoSliderPosition;
+			PlayerEvents.Instance.OnGunChanged -= UpdateCurrentGunAmmoCount;
+		}
+
+		private void UpdateAmmoSliderPosition(int _currentAmmo)
+		{
+			ammoSlider.value = (float)_currentAmmo / currentGunAmmoCount;
+		}
+
+		private void UpdateCurrentGunAmmoCount(int _newGunAmmoCount)
+		{
+			currentGunAmmoCount = _newGunAmmoCount - 1;
+		}
+	}
+}
