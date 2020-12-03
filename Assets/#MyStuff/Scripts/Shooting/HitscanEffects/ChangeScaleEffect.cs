0f1048e9ac380e57ee751f4a9b3ddb09322068ab
Yoshua Woo <yoshua.woo@gmail.com>
2020-12-03 18:35:53 +0100

    WIP change scale effect

---
+++ Assets/#MyStuff/Scripts/Shooting/HitscanEffects/ChangeScaleEffect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+namespace NonViolentFPS.Shooting
+{
+	public class ChangeScaleEffect : MonoBehaviour, IHitscanEffect
+	{
+		[SerializeField] private float changeAmount;
+
+		public void Initialize(RaycastHit _hit)
+		{
+			ChangeScale(changeAmount);
+		}
+
+		public void Destroy()
+		{
+			ChangeScale(-changeAmount);
+		}
+
+		private void ChangeScale(float _changeAmount)
+		{
+			var parentTransform = transform.parent;
+			if(parentTransform.localScale.magnitude >= changeAmount)
+			{
+				parentTransform.localScale += Vector3.one * _changeAmount;
+			}
+			else if(parentTransform.localScale.magnitude <)
+		}
+	}
+}
