a0c61076a337ffea3774c9da39713e769733d03a
Yoshua Woo <yoshua.woo@gmail.com>
2020-12-08 12:39:13 +0100

    Scene Setup Loader ScriptableObject

---
+++ Assets/#MyStuff/Scripts/SceneManagement/SceneSetupLoader.cs
@@ -0,0 +1,24 @@
+﻿using Sirenix.OdinInspector;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace NonViolentFPS.SceneManagement
+{
+	[CreateAssetMenu(fileName = "SceneLoader", menuName = "SceneManagement/Loader", order = 0)]
+	public class SceneSetupLoader : SerializedScriptableObject
+	{
+		private SceneSetup[] setup;
+
+		[Button("Save Scene Setup", ButtonSizes.Large)]
+		private void SaveConfig()
+		{
+			setup = EditorSceneManager.GetSceneManagerSetup();
+		}
+
+		[Button("Load Scene Setup", ButtonSizes.Large)]
+		private void LoadConfig()
+		{
+			EditorSceneManager.RestoreSceneManagerSetup(setup);
+		}
+	}
+}
