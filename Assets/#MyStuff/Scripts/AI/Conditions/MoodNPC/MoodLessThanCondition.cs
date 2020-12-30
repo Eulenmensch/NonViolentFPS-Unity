bc2b1e6af85789994860de2053ef01e70e915687
Yoshua Woo <yoshua.woo@gmail.com>
2020-12-30 20:26:52 +0100

    Mood NPC AI assets

---
+++ Assets/#MyStuff/Scripts/AI/Conditions/MoodNPC/MoodLessThanCondition.cs
@@ -0,0 +1,22 @@
+using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(menuName = "AI Kit/Conditions/Mood/MoodLessThanCondition")]
+	public class MoodLessThanCondition : Condition
+	{
+		[SerializeField] private float lessThanValue;
+		public override bool Evaluate(NPC _npc)
+		{
+			var moodNPC = _npc as MoodNPC;
+			if (moodNPC == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(MoodNPC));
+				return false;
+			}
+
+			return moodNPC.Mood < lessThanValue;
+		}
+	}
+}
