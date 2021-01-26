b6d460d7c41667ba0829ecb28e191f7dcd5aa399
Yoshua Woo <yoshua.woo@gmail.com>
2021-01-26 18:17:55 +0100

    Fixed the interaction where only one of the bad mood NPCs would start fighting

---
+++ Assets/#MyStuff/Scripts/AI/Conditions/MoodNPC/NearbyNPCFightingCondition.cs
@@ -0,0 +1,30 @@
+﻿using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(fileName = "NearbyNPCFightingCondition", menuName = "AI Kit/Conditions/Mood/NearbyNPCFightingCondition")]
+	public class NearbyNPCFightingCondition : Condition
+	{
+		[SerializeField] private State fightingState;
+
+		public override bool Evaluate(NPC _npc)
+		{
+			var otherNPCsComponent = _npc as IOtherNPCsComponent;
+			if (otherNPCsComponent == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(IOtherNPCsComponent));
+				return false;
+			}
+
+			foreach (var npc in otherNPCsComponent.OtherNPCs)
+			{
+				if (npc.StateMachine.CurrentState == fightingState)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
