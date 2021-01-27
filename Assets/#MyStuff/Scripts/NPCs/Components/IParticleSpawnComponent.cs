66ddc39da74cef4c0d227acc177b32f773d7f06e
Yoshua Woo <yoshua.woo@gmail.com>
2021-01-27 18:18:28 +0100

    Particle Spawn NPC Component

---
+++ Assets/#MyStuff/Scripts/NPCs/Components/IParticleSpawnComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NonViolentFPS.NPCs
+{
+	public interface IParticleSpawnComponent
+	{
+		Transform SpawnPoint { get; set; }
+		HashSet<GameObject> Particles { get; set; }
+	}
+}
