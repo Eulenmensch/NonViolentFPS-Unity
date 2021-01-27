710fce6df09f1c1d425777b5cd259def7df72f7b
Yoshua Woo <yoshua.woo@gmail.com>
2021-01-27 18:17:16 +0100

    Spawn and Despawn Particle Enter and Exit Actions

---
+++ Assets/#MyStuff/Scripts/AI/Enter Actions/SpawnParticlesEnterAction.cs
@@ -0,0 +1,25 @@
+﻿using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(fileName = "SpawnParticlesEnterAction", menuName = "AI Kit/Enter Actions/SpawnParticlesEnterAction")]
+	public class SpawnParticlesEnterAction : EnterAction
+	{
+		[SerializeField] private GameObject ParticlePrefab;
+
+		public override void Enter(NPC _npc)
+		{
+			var particleSpawnComponent = _npc as IParticleSpawnComponent;
+			if (particleSpawnComponent == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(IParticleSpawnComponent));
+				return;
+			}
+
+			var spawnPoint = particleSpawnComponent.SpawnPoint;
+			var particles = Instantiate(ParticlePrefab, spawnPoint.position, Quaternion.identity, spawnPoint);
+			particleSpawnComponent.Particles.Add(particles);
+		}
+	}
+}
---
+++ Assets/#MyStuff/Scripts/AI/Exit Actions/DespawnParticlesExitAction.cs
@@ -0,0 +1,24 @@
+﻿using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(fileName = "DespawnParticlesExitAction", menuName = "AI Kit/Exit Actions/DespawnParticlesExitAction")]
+	public class DespawnParticlesExitAction : ExitAction
+	{
+		public override void Exit(NPC _npc)
+		{
+			var particleSpawnComponent = _npc as IParticleSpawnComponent;
+			if (particleSpawnComponent == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(IParticleSpawnComponent));
+				return;
+			}
+
+			foreach (var particle in particleSpawnComponent.Particles)
+			{
+				Destroy(particle);
+			}
+		}
+	}
+}
