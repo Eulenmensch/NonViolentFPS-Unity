ef321834d48397118af6e3618591a28126edb847
Yoshua Woo <yoshua.woo@gmail.com>
2021-01-28 18:04:49 +0100

    Game Modes
    Started working on an abstract game mode that should probably be an interface;
    Hacked together a prototypey Gamemode for the mood prototype;

---
+++ Assets/#MyStuff/Scripts/GameModes/GameMode.cs
@@ -0,0 +1,11 @@
+﻿using NonViolentFPS.SceneManagement;
+using UnityEngine;
+
+namespace NonViolentFPS.GameModes
+{
+	public abstract class GameMode
+	{
+		[field: SerializeField] public SceneSetupLoader SceneSetup { get; set; }
+		
+	}
+}
---
+++ Assets/#MyStuff/Scripts/GameModes/MoodyNPCMode.cs
@@ -0,0 +1,56 @@
+﻿using NonViolentFPS.Manager;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace NonViolentFPS.GameModes
+{
+	public class MoodyNPCMode : SerializedMonoBehaviour
+	{
+		#region Singleton
+		public static MoodyNPCMode Instance { get; private set; }
+
+		private void Awake()
+		{
+			if ( Instance != null && Instance != this )
+			{
+				Destroy( this );
+			}
+			else
+			{
+				Instance = this;
+			}
+		}
+		#endregion
+
+		[SerializeField] private int maxUnitsInFights;
+		[SerializeField] private float gameDuration;
+
+		private float time;
+		private int activeUnitsInFights;
+		private bool won;
+
+		private void Update()
+		{
+			time += Time.deltaTime;
+			if (time >= gameDuration && !won)
+			{
+				GameManager.Instance.SetGameWon();
+				won = true;
+			}
+		}
+
+		public void AddUnitInFight()
+		{
+			activeUnitsInFights++;
+			if (activeUnitsInFights >= maxUnitsInFights)
+			{
+				GameManager.Instance.SetGameLost();
+			}
+		}
+
+		public void RemoveUnitInFight()
+		{
+			activeUnitsInFights--;
+		}
+	}
+}
