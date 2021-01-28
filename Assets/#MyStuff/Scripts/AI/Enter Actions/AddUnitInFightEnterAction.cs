4e6e05d69708df42f76731fb81ceae893f8c47f9
Yoshua Woo <yoshua.woo@gmail.com>
2021-01-28 18:05:08 +0100

    Enter and Exit Actions for Mood Prototype Game State

---
+++ Assets/#MyStuff/Scripts/AI/Enter Actions/AddUnitInFightEnterAction.cs
@@ -0,0 +1,15 @@
+﻿using NonViolentFPS.GameModes;
+using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(fileName = "AddUnitInFightEnterAction", menuName = "AI Kit/Enter Actions/AddUnitInFightEnterAction")]
+	public class AddUnitInFightEnterAction : EnterAction
+	{
+		public override void Enter(NPC _npc)
+		{
+			MoodyNPCMode.Instance.AddUnitInFight();
+		}
+	}
+}
---
+++ Assets/#MyStuff/Scripts/AI/Exit Actions/RemoveUnitInFightExitAction.cs
@@ -0,0 +1,15 @@
+﻿using NonViolentFPS.GameModes;
+using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(fileName = "RemoveUnitInFightExitAction", menuName = "AI Kit/Exit Actions/RemoveUnitInFightExitAction")]
+	public class RemoveUnitInFightExitAction : ExitAction
+	{
+		public override void Exit(NPC _npc)
+		{
+			MoodyNPCMode.Instance.RemoveUnitInFight();
+		}
+	}
+}
