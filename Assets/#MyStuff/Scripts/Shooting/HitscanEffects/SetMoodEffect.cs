018e4c88074a8fb5c9b71635e7281471a42ba0ba
Yoshua Woo <yoshua.woo@gmail.com>
2021-01-28 13:54:25 +0100

    Set Mood to specific value hitscan effect

---
+++ Assets/#MyStuff/Scripts/Shooting/HitscanEffects/SetMoodEffect.cs
@@ -0,0 +1,30 @@
+﻿using NonViolentFPS.NPCs;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace NonViolentFPS.Shooting
+{
+	public class SetMoodEffect : SerializedMonoBehaviour, IHitscanEffect
+	{
+		[SerializeField] private float mood;
+		
+		public void Initialize(RaycastHit _hit)
+		{
+			var moodNPC = GetComponentInParent<MoodNPC>(); //Because only the main body collider object has a MoodNPC component attached
+			if (moodNPC == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(MoodNPC));
+				return;
+			}
+
+			moodNPC.Mood = mood;
+
+			Destroy();
+		}
+
+		public void Destroy()
+		{
+			Destroy(gameObject);
+		}
+	}
+}
