836bb253bde9a5b03e0643428d3d3ad5e25f03e4
Yoshua Woo <yoshua.woo@gmail.com>
2021-02-04 13:40:53 +0100

    Foot IK Scripts

---
+++ Assets/#MyStuff/Scripts/IK/FootIKTarget.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace NonViolentFPS.IK
+{
+	public class FootIKTarget : MonoBehaviour
+	{
+		public float MaxFootHeight;
+		public float TweenTime;
+		public float MaxDist;
+		public Transform FootPos;
+
+		void Start()
+		{
+			MaxDist *= 1.5f;
+		}
+
+		void FixedUpdate()
+		{
+			var distanceRatio = Mathf.Clamp( ( ( FootPos.position - transform.position ).magnitude / MaxDist ), 0, 1 );
+
+			var height = Mathf.Lerp( 0, MaxFootHeight, distanceRatio );
+
+			if ( ( FootPos.position - transform.position ).magnitude >= MaxDist )
+			{
+				transform.DOMove( FootPos.position, TweenTime ).SetEase( Ease.InOutQuad );
+				// transform.DOPunchPosition( MaxFootHeight * Vector3.up, TweenTime, 0, 0, false );
+			}
+		}
+	}
+}
---
+++ Assets/#MyStuff/Scripts/IK/FootIKTargetCast.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace NonViolentFPS.IK
+{
+	public class FootIKTargetCast : MonoBehaviour
+	{
+		public float castHeight;
+		public float offset;
+		public LayerMask layerMask;
+		public float yPos;
+		public RaycastHit hit;
+
+		void FixedUpdate()
+		{
+			Vector3 castPoint = new Vector3( transform.position.x, transform.parent.position.y + castHeight, transform.position.z );
+			if ( UnityEngine.Physics.Raycast( castPoint, Vector3.down, out hit, Mathf.Infinity, layerMask ) )
+			{
+				yPos = hit.point.y + offset;
+				transform.position = new Vector3( transform.position.x, yPos, transform.position.z );
+			}
+		}
+
+		private void OnDrawGizmos()
+		{
+			Gizmos.color = Color.magenta;
+			Gizmos.DrawCube( transform.position, Vector3.one * 0.4f );
+		}
+	}
+}
