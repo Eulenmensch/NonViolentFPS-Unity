f0312c771d4aba786879290c93a9b3d8042c92c9
Yoshua Woo <yoshua.woo@gmail.com>
2021-02-09 18:15:48 +0100

    Game Events

---
+++ Assets/#MyStuff/Scripts/Events/GameEvents.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+namespace NonViolentFPS.Events
+{
+	public class GameEvents : MonoBehaviour
+	{
+		#region Singleton
+		public static GameEvents Instance { get; private set; }
+
+		private void Awake()
+		{
+			if ( Instance != null && Instance != this )
+			{
+				Destroy( this );
+			}
+			else
+			{
+				Instance = this;
+			}
+		}
+		#endregion
+
+		public event Action OnGameWon;
+		public void GameWon(){OnGameWon?.Invoke();}
+
+		public event Action OnGameLost;
+		public void GameLost(){OnGameLost?.Invoke();}
+
+		public event Action<int> OnScoreChanged;
+		public void ChangeScore(int _scoreChange){OnScoreChanged?.Invoke(_scoreChange);}
+	}
+}
