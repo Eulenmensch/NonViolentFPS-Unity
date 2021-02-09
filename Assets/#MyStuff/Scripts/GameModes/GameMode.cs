a99cc2fbd17563a8c7750609773faa39bf1695dc
Yoshua Woo <yoshua.woo@gmail.com>
2021-02-09 18:15:30 +0100

    Game Mode Changes
    now has an array of scene references;
    changed to scriptable object;

--- Assets/#MyStuff/Scripts/GameModes/GameMode.cs
+++ Assets/#MyStuff/Scripts/GameModes/GameMode.cs
@@ -1,11 +1,28 @@
-﻿using NonViolentFPS.SceneManagement;
+﻿using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace NonViolentFPS.GameModes
 {
-	public abstract class GameMode
+	public abstract class GameMode : SerializedScriptableObject
 	{
-		[field: SerializeField] public SceneSetupLoader SceneSetup { get; set; }
-		
+		[SerializeField] private SceneReference[] scenes;
+
+		public int Score { get; private set; }
+
+		protected virtual void Initialize()
+		{
+			foreach (var scene in scenes)
+			{
+				SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+			}
+		}
+
+		protected abstract void Evaluate();
+
+		protected virtual void ChangeScore(int _scoreChange)
+		{
+			Score += _scoreChange;
+		}
 	}
 }
