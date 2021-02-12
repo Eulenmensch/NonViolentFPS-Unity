719ddac313e28394fc2223bcec0d1873655b3bfd
Yoshua Woo <yoshua.woo@gmail.com>
2021-02-12 17:54:52 +0100

    Test Game Mode that only loads the player and HUD

---
+++ Assets/#MyStuff/Scripts/GameModes/TestMode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace NonViolentFPS.GameModes
+{
+	[CreateAssetMenu(fileName = "TestMode", menuName = "GameModes/Test")]
+	public class TestMode : GameMode
+	{
+		public override void Evaluate()
+		{
+			//Testing...
+		}
+	}
+}
