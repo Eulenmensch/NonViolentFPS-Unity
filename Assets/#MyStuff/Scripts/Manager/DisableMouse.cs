f23365dd089b6bdb24fc295b6813857b29b7a985
Yoshua Woo <yoshua.woo@gmail.com>
2021-02-20 02:00:23 +0100

    Cursor and mouse locking now happens by subscribing to game events

--- Assets/#MyStuff/Scripts/Manager/DisableMouse.cs
+++ Assets/#MyStuff/Scripts/Manager/DisableMouse.cs
@@ -1,13 +1,41 @@
+using System;
+using NonViolentFPS.Events;
 using UnityEngine;
 
 namespace NonViolentFPS.Manager
 {
     public class DisableMouse : MonoBehaviour
     {
+        private void OnEnable()
+        {
+            GameEvents.Instance.OnGameLost += UnlockCursor;
+            GameEvents.Instance.OnGameWon += UnlockCursor;
+            GameEvents.Instance.OnGameRestarted += LockCursor;
+        }
+
+        private void OnDisable()
+        {
+            GameEvents.Instance.OnGameLost -= UnlockCursor;
+            GameEvents.Instance.OnGameWon -= UnlockCursor;
+            GameEvents.Instance.OnGameRestarted -= LockCursor;
+        }
+
         private void Start()
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void LockCursor()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void UnlockCursor()
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 }
