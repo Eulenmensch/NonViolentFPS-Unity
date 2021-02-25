e6496a9e4f7b5a5f4671d17a15a5ed6684e65f61
Yoshua Woo <yoshua.woo@gmail.com>
2021-02-25 20:07:06 +0100

    Bubble Freezer and Ventilator Behaviours

---
+++ Assets/#MyStuff/Scripts/Level/BubbleFreezer.cs
@@ -0,0 +1,21 @@
+﻿using NonViolentFPS.Shooting;
+using UnityEngine;
+
+public class BubbleFreezer : MonoBehaviour
+{
+	[SerializeField] private float unfreezeTime;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		var enclosingProjectile = other.GetComponent<EnclosingProjectile>();
+		if (enclosingProjectile == null) {return;}
+		enclosingProjectile.Freeze();
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		var enclosingProjectile = other.GetComponent<EnclosingProjectile>();
+		if (enclosingProjectile == null) {return;}
+		enclosingProjectile.Unfreeze(unfreezeTime);
+	}
+}
---
+++ Assets/#MyStuff/Scripts/Level/Ventilator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Ventilator : MonoBehaviour
+{
+    [SerializeField] private float pushForce;
+    [SerializeField, Range(0.1f,10f)] private float forceDistanceFalloff;
+
+    private HashSet<Rigidbody> pushableRigidbodies;
+
+    private void Start()
+    {
+        pushableRigidbodies = new HashSet<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var pushableRigidbody in pushableRigidbodies)
+        {
+            var distance = Vector3.Distance(pushableRigidbody.position, transform.position);
+            var distanceFalloff = 1 + forceDistanceFalloff * distance;
+            //This makes for a Hyperbola where forceDistanceFalloff is the dilating factor
+            var resultingForce = transform.up * (pushForce/distanceFalloff);
+            //This force should depend on the mass of the pushed body
+            pushableRigidbody.AddForce(resultingForce, ForceMode.Force);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var otherRigidbody = other.attachedRigidbody;
+        if (otherRigidbody == null || otherRigidbody.isKinematic) {return;}
+
+        pushableRigidbodies.Add(otherRigidbody);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var otherRigidbody = other.attachedRigidbody;
+        if (otherRigidbody == null || otherRigidbody.isKinematic) {return;}
+
+        pushableRigidbodies.Remove(otherRigidbody);
+    }
+}
