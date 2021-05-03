08f935a23c7174429c5f22fffc7032b4c19644e9
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-03 16:51:25 +0200

    Default Location Component

---
+++ Assets/#MyStuff/Scripts/NPCs/Components/IDefaultLocationComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace NonViolentFPS.NPCs
+{
+	public interface IDefaultLocationComponent
+	{
+		Vector3 DefaultLocation { get; set; }
+		float BufferRadiusMin { get; set; }
+		float BufferRadiusMax { get; set; }
+	}
+}
