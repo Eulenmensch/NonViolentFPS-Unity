a74ba3b9353a19953f2a4998c93c573829c8d89b
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-03 18:29:06 +0200

    Move to default location behaviour

---
+++ Assets/#MyStuff/Scripts/AI/Behaviours/MoveToDefaultLocationBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(menuName = "AI Kit/Behaviours/MoveToDefaultLocationBehaviour")]
+	public class MoveToDefaultLocationBehaviour : AIBehaviour
+	{
+		public override UpdateType type => UpdateType.Regular;
+
+		public override void DoBehaviour(NPC _npc)
+		{
+			var defaultLocationComponent = _npc as IDefaultLocationComponent;
+			if (defaultLocationComponent == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(IDefaultLocationComponent));
+				return;
+			}
+
+			var navMeshComponent = _npc as INavMeshMoveComponent;
+			if (navMeshComponent == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(INavMeshMoveComponent));
+				return;
+			}
+
+			var distance = Vector3.Distance(navMeshComponent.Agent.destination, defaultLocationComponent.DefaultLocation);
+
+			if (distance >= defaultLocationComponent.BufferRadiusMax)
+			{
+				navMeshComponent.Agent.SetDestination(defaultLocationComponent.DefaultLocation);
+			}
+		}
+	}
+}
