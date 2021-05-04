c91ed0c73d9d1fbd3d9f449d75d8d19a35768828
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-04 17:33:37 +0200

    new player within radius condition and assets

---
+++ Assets/#MyStuff/Scripts/AI/Conditions/PlayerWithinRadiusCondition.cs
@@ -0,0 +1,21 @@
+﻿using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu( menuName = "AI Kit/Conditions/PlayerWithinRadiusCondition" )]
+	public class PlayerWithinRadiusCondition : Condition
+	{
+		public override UpdateType type => UpdateType.Regular;
+
+		[SerializeField] private float range;
+
+		public override bool Evaluate(NPC _npc)
+		{
+			var playerPosition = _npc.Player.transform.position;
+			var selfPosition = _npc.transform.position;
+
+			return Vector3.Distance(playerPosition, selfPosition) <= range;
+		}
+	}
+}
