f46d8310cc55a8e8caee42fa596e4d3be76d0bd0
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-05 20:58:18 +0200

    Added new Components to the Grappler Enemy;
    Timer for Wait State;
    Particle Spawn for Alert Exclamation Particles;

--- Assets/#MyStuff/Scripts/NPCs/Compound NPCs/GrapplerEnemyNPC.cs
+++ Assets/#MyStuff/Scripts/NPCs/Compound NPCs/GrapplerEnemyNPC.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.AI;
 
 namespace NonViolentFPS.NPCs
 {
 	public class GrapplerEnemyNPC : NPC,
-		IChaseComponent,IDefaultLocationComponent,IRangeComponent,INavMeshMoveComponent,IRigidbodyComponent,IJumpComponent,IGroundCheckComponent,IHeadComponent
+		IChaseComponent,IDefaultLocationComponent,IRangeComponent,INavMeshMoveComponent,IRigidbodyComponent,IJumpComponent,IGroundCheckComponent,IHeadComponent,ITimerComponent,IParticleSpawnComponent
 	{
 		public Vector3 LastKnownPlayerLocation { get; set; }
 		public Vector3 DefaultLocation { get; set; }
@@ -19,11 +21,24 @@ namespace NonViolentFPS.NPCs
 		[field: SerializeField] public Vector3 JumpDirection { get; set; }
 		public bool Grounded { get; set; }
 		[field: SerializeField] public Transform Head { get; set; }
+		public float Timer { get; set; }
+		[field: SerializeField] public float MinTime { get; set; }
+		[field: SerializeField] public float MaxTime { get; set; }
+		[field: SerializeField] public Transform SpawnPoint { get; set; }
+		public HashSet<GameObject> Particles { get; set; } = new HashSet<GameObject>();
 
 		protected override void Start()
 		{
 			base.Start();
 			DefaultLocation = transform.position;
+		}
+
+		private void OnDrawGizmos()
+		{
+			if (Camera.current != Camera.main && Camera.current != SceneView.lastActiveSceneView.camera) return;
+
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawSphere(DefaultLocation, 0.3f);
 		}
 	}
 }
