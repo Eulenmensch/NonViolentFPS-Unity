acfc82141e664d31406f6142936341172fe45067
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-10 15:28:19 +0200

    AmmoCounter UI monobehaviour
    subscribes to ammo text UI event;

---
+++ Assets/#MyStuff/Scripts/UI/AmmoCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NonViolentFPS.Events;
+using TMPro;
+using UnityEngine;
+
+namespace NonViolentFPS.UI
+{
+	public class AmmoCounter : MonoBehaviour
+	{
+		[SerializeField] private TMP_Text ammoCounterText;
+
+
+		private void OnEnable()
+		{
+			UIEvents.Instance.OnAmmoTextUpdate += UpdateAmmoCounter;
+		}
+
+		private void OnDisable()
+		{
+			UIEvents.Instance.OnAmmoTextUpdate -= UpdateAmmoCounter;
+		}
+
+		private void UpdateAmmoCounter(string _ammoCount)
+		{
+			ammoCounterText.text = _ammoCount;
+		}
+	}
+}
