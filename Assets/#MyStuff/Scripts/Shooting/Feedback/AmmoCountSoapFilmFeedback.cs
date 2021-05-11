34f39979671f27a216af95bf5f3ca1bd4bc6ae31
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-11 17:43:49 +0200

    soap film feedback when depleting ammo

---
+++ Assets/#MyStuff/Scripts/Shooting/Feedback/AmmoCountSoapFilmFeedback.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using NonViolentFPS.Events;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace NonViolentFPS.Shooting
+{
+	public class AmmoCountSoapFilmFeedback : SerializedMonoBehaviour
+	{
+		[SerializeField] private Material soapFilmMaterial;
+		[DictionaryDrawerSettings(KeyLabel = "ammoCount", ValueLabel = "Posterize Power")]
+		public Dictionary<int, float> ammoCountPosterizationDictionary;
+
+		private float defaultOpacity;
+
+		private void OnEnable()
+		{
+			UIEvents.Instance.OnAmmoTextUpdate += UpdateSoapFilmFeedback;
+		}
+
+		private void OnDisable()
+		{
+			UIEvents.Instance.OnAmmoTextUpdate -= UpdateSoapFilmFeedback;
+		}
+
+		private void Start()
+		{
+			defaultOpacity = soapFilmMaterial.GetFloat("_Opacity");
+		}
+
+		private void UpdateSoapFilmFeedback(int _ammoCount)
+		{
+			var posterizePower = ammoCountPosterizationDictionary[_ammoCount];
+			soapFilmMaterial.SetFloat("_PosterizePower", posterizePower);
+			if (_ammoCount == 0)
+			{
+				soapFilmMaterial.SetFloat("_Opacity", 0);
+			}
+			else
+			{
+				soapFilmMaterial.SetFloat("_Opacity", defaultOpacity);
+			}
+		}
+	}
+}
