da52c6c8a3c30a573daee5b33e3049c71fb1b747
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-11 12:41:57 +0200

    Changed ammotext event type to int for ammocounter logic
    ammocounter changes sprite based on ammo fill percentage;

--- Assets/#MyStuff/Scripts/Events/UIEvents.cs
+++ Assets/#MyStuff/Scripts/Events/UIEvents.cs
@@ -25,7 +25,7 @@ namespace NonViolentFPS.Events
 		public event Action<string> OnTimerTextUpdate;
 		public void UpdateTimerText(string _timerText){OnTimerTextUpdate?.Invoke(_timerText);}
 
-		public event Action<string> OnAmmoTextUpdate;
-		public void UpdateAmmoText(string _ammoText){OnAmmoTextUpdate?.Invoke(_ammoText);}
+		public event Action<int> OnAmmoTextUpdate;
+		public void UpdateAmmoText(int _ammoCount){OnAmmoTextUpdate?.Invoke(_ammoCount);}
 	}
 }
--- Assets/#MyStuff/Scripts/UI/AmmoCounter.cs
+++ Assets/#MyStuff/Scripts/UI/AmmoCounter.cs
@@ -1,28 +1,64 @@
-﻿using System;
-using NonViolentFPS.Events;
+﻿using NonViolentFPS.Events;
+using NonViolentFPS.Manager;
+using NonViolentFPS.Shooting;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace NonViolentFPS.UI
 {
 	public class AmmoCounter : MonoBehaviour
 	{
 		[SerializeField] private TMP_Text ammoCounterText;
+		[SerializeField] private Image outlineImage;
+		[SerializeField] private Sprite[] outlines;
 
+		private int clipSize;
 
 		private void OnEnable()
 		{
 			UIEvents.Instance.OnAmmoTextUpdate += UpdateAmmoCounter;
+			UIEvents.Instance.OnAmmoTextUpdate += UpdateAmmoCounterOutline;
 		}
 
 		private void OnDisable()
 		{
 			UIEvents.Instance.OnAmmoTextUpdate -= UpdateAmmoCounter;
+			UIEvents.Instance.OnAmmoTextUpdate -= UpdateAmmoCounterOutline;
 		}
 
-		private void UpdateAmmoCounter(string _ammoCount)
+		private void Start()
 		{
-			ammoCounterText.text = _ammoCount;
+			var ammoClipComponent = GameManager.Instance.Player.GetComponent<ShooterCopy>().ActiveGun as IAmmoClipComponent;
+			if (ammoClipComponent == null)
+			{
+				Debug.LogError("the active gun does not have an ammo clip");
+				return;
+			}
+			clipSize = ammoClipComponent.ClipSize;
+			UIEvents.Instance.UpdateAmmoText(clipSize);
+		}
+
+		private void UpdateAmmoCounter(int _ammoCount)
+		{
+			ammoCounterText.text = _ammoCount.ToString();
+		}
+
+		private void UpdateAmmoCounterOutline(int _ammoCount)
+		{
+			var ammoPercent = (float)_ammoCount / clipSize;
+			if (ammoPercent >= 0.34f)
+			{
+				outlineImage.sprite = outlines[0];
+			}
+			else if (ammoPercent > 0)
+			{
+				outlineImage.sprite = outlines[1];
+			}
+			else if (ammoPercent == 0)
+			{
+				outlineImage.sprite = outlines[2];
+			}
 		}
 	}
 }
