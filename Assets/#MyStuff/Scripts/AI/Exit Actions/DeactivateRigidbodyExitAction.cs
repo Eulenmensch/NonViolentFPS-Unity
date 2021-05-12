2079ad48d0410baca481c46637a2db4566ae71d4
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-12 19:14:55 +0200

    Exit Actions for rigidbody and navmeshagent

---
+++ Assets/#MyStuff/Scripts/AI/Exit Actions/DeactivateRigidbodyExitAction.cs
@@ -0,0 +1,21 @@
+﻿using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(fileName = nameof(DeactivateRigidbodyExitAction), menuName = "AI Kit/Exit Actions/DeactivateRigidbodyExitAction")]
+	public class DeactivateRigidbodyExitAction : ExitAction
+	{
+		public override void Exit(NPC _npc)
+		{
+			var rigidbodyComponent = _npc as IRigidbodyComponent;
+			if (rigidbodyComponent == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(IRigidbodyComponent));
+				return;
+			}
+
+			rigidbodyComponent.RigidbodyRef.isKinematic = true;
+		}
+	}
+}
---
+++ Assets/#MyStuff/Scripts/AI/Exit Actions/EnableNavMeshAgentExitAction.cs
@@ -0,0 +1,23 @@
+﻿using NonViolentFPS.NPCs;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(fileName = nameof(EnableNavMeshAgentExitAction), menuName = "AI Kit/Exit Actions/EnableNavMeshExitAction")]
+	public class EnableNavMeshAgentExitAction : ExitAction
+	{
+		public override void Exit(NPC _npc)
+		{
+			var navMeshMoveComponent = _npc as INavMeshMoveComponent;
+			if (navMeshMoveComponent == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(INavMeshMoveComponent));
+				return;
+			}
+
+			var navMeshAgent = navMeshMoveComponent.Agent;
+			if (navMeshAgent == null) return;
+			navMeshAgent.enabled = true;
+		}
+	}
+}
