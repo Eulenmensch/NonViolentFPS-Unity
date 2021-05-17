ad199caa2645daf0fe474676f33e8b5d78aef6f2
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-17 18:39:58 +0200

    Grappler Projectile

---
+++ Assets/#MyStuff/Scripts/Shooting/Projectiles/GrapplerEnemyProjectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace NonViolentFPS.Shooting
+{
+	public class GrapplerEnemyProjectile : PhysicsProjectile
+	{
+		[SerializeField] private GameObject grapplerPrefab;
+
+		protected override void ImpactAction(Collision _other)
+		{
+			SpawnGrappler();
+		}
+
+		private void SpawnGrappler()
+		{
+			Instantiate(grapplerPrefab, transform.position, Quaternion.identity);
+		}
+	}
+}
