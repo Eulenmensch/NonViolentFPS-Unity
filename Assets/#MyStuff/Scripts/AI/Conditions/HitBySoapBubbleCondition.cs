3a4c54f612c63c4ea75a346d6dc12b52e529e94a
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-19 12:46:22 +0200

    HitBySoapBubble Condition

---
+++ Assets/#MyStuff/Scripts/AI/Conditions/HitBySoapBubbleCondition.cs
@@ -0,0 +1,24 @@
+﻿using NonViolentFPS.NPCs;
+using NonViolentFPS.Shooting;
+using UnityEngine;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(menuName = "AI Kit/Conditions/HitBySoapBubbleCondition")]
+	public class HitBySoapBubbleCondition : Condition
+	{
+		public override UpdateType type => UpdateType.Physics;
+
+		public override bool Evaluate(NPC _npc)
+		{
+			foreach (var collision in _npc.ActiveCollisions)
+			{
+				if (collision.gameObject.GetComponent<EnclosingProjectile>() != null)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
