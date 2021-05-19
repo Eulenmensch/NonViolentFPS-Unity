9197d994c8ea560fa79410dfb488d6227ce3a279
Yoshua Woo <yoshua.woo@gmail.com>
2021-05-19 16:38:00 +0200

    behaviour and condition to get Grapplers back onto the navmesh

---
+++ Assets/#MyStuff/Scripts/AI/Behaviours/ApplyForceToNearestNavMeshBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using NonViolentFPS.NPCs;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(menuName = "AI Kit/Behaviours/ApplyForceToNearestNavMeshBehaviour")]
+	public class ApplyForceToNearestNavMeshBehaviour : AIBehaviour
+	{
+		[SerializeField] private string areaName;
+		[SerializeField] private float forceAmount;
+
+		public override UpdateType type => UpdateType.Physics;
+
+		public override void DoBehaviour(NPC _npc)
+		{
+			var rigidbodyComponent = _npc as IRigidbodyComponent;
+			if (rigidbodyComponent == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(IRigidbodyComponent));
+				return;
+			}
+
+			var npcPosition = _npc.transform.position;
+			NavMesh.FindClosestEdge(npcPosition, out var hit, NavMesh.AllAreas);
+			var directionToNavMesh = (hit.position - npcPosition).normalized;
+			var horizontalDirectionToNavMesh = new Vector3(directionToNavMesh.x, 0, directionToNavMesh.z);
+
+			if(horizontalDirectionToNavMesh.magnitude > 0)
+			{
+				var npcRigidbody = rigidbodyComponent.RigidbodyRef;
+				npcRigidbody.AddForce(horizontalDirectionToNavMesh * forceAmount, ForceMode.Acceleration);
+			}
+		}
+	}
+}
---
+++ Assets/#MyStuff/Scripts/AI/Conditions/OnNavMeshCondition.cs
@@ -0,0 +1,25 @@
+﻿using NonViolentFPS.NPCs;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace NonViolentFPS.AI
+{
+	[CreateAssetMenu(menuName = "AI Kit/Conditions/OnNavMeshCondition")]
+	public class OnNavMeshCondition : Condition
+	{
+		public override UpdateType type => UpdateType.Regular;
+
+		public override bool Evaluate(NPC _npc)
+		{
+			var navMeshComponent = _npc as INavMeshMoveComponent;
+			if (navMeshComponent == null)
+			{
+				NPC.ThrowComponentMissingError(typeof(INavMeshMoveComponent));
+				return false;
+			}
+
+			var maxSearchRadius = navMeshComponent.Agent.height *2;
+			return NavMesh.SamplePosition(_npc.transform.position, out var hit, maxSearchRadius, NavMesh.AllAreas);
+		}
+	}
+}
